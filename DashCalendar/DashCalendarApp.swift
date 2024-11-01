bc174888e0119c7b558b7af010cf9af597eec807
Nikita <nnnkbrrr@icloud.com>
2024-11-01 17:12:27 +0300

    Initial Commit

---
+++ DashCalendar/DashCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  DashCalendarApp.swift
+//  DashCalendar
+//
+//  Created by Nikita on 11/1/24.
+//
+
+import SwiftUI
+
+@main
+struct DashCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
